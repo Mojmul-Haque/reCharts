f6c76ed922a7f392231f41e14a7ab2affba0c5d5
Mojmul Haque <mojmul150@gmail.com>
2021-03-04 11:09:04 +0600

    add pie charts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css'
-import AreaCharts from './Components/PiChart/AreaCharts/AreaChart';
+import AreaCharts from './Components/PiChart/AreaCharts/AreaChart'
+import PiChart from './Components/PiChart/PiChart'
+import ShapePieChart from './Components/PiChart/ShapePieChart/ShapePieChart'
+import BarAreaChart from './Components/PiChart/SimpleSaleryChart/InitializeChart'
 import SalaryChart from './Components/PiChart/SimpleSaleryChart/SalaryChart'
-
 
 function App () {
   return (
-    
     <div className='App'>
-     <SalaryChart></SalaryChart>
-     <AreaCharts></AreaCharts>
+      <SalaryChart></SalaryChart>
+      <AreaCharts></AreaCharts>
+      <div style={{ display: 'flex', justifyContent: 'center', margin:' 50px 0'}}>
+        <BarAreaChart></BarAreaChart>
+        <PiChart></PiChart>
+        <ShapePieChart></ShapePieChart>
+      </div>
     </div>
   )
 }
